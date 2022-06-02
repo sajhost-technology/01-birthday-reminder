282f58ee42689d35b98c048dc957ec018debbb0d
AMRIT <ca@shahiamrit365.com.np>
2022-06-02 22:02:28 +0545

    added

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 1,
+        name: "Amrit Shahi",
+        age: 27,
+        image: 'https://media-exp1.licdn.com/dms/image/C4D03AQGFX0OOa4DOUQ/profile-displayphoto-shrink_200_200/0/1595253508875?e=1655337600&v=beta&t=T1ctnwkueEzOzH25c4jl1IfWG0DsgB-CdYdbX2PUc70'
+    },
+
+    {
+        id: 1,
+        name: "Amrit Shahi",
+        age: 27,
+        image: 'https://media-exp1.licdn.com/dms/image/C4D03AQGFX0OOa4DOUQ/profile-displayphoto-shrink_200_200/0/1595253508875?e=1655337600&v=beta&t=T1ctnwkueEzOzH25c4jl1IfWG0DsgB-CdYdbX2PUc70'
+    },
+
+    {
+        id: 1,
+        name: "Amrit Shahi",
+        age: 27,
+        image: 'https://media-exp1.licdn.com/dms/image/C4D03AQGFX0OOa4DOUQ/profile-displayphoto-shrink_200_200/0/1595253508875?e=1655337600&v=beta&t=T1ctnwkueEzOzH25c4jl1IfWG0DsgB-CdYdbX2PUc70'
+    },
+
+    {
+        id: 1,
+        name: "Amrit Shahi",
+        age: 27,
+        image: 'https://media-exp1.licdn.com/dms/image/C4D03AQGFX0OOa4DOUQ/profile-displayphoto-shrink_200_200/0/1595253508875?e=1655337600&v=beta&t=T1ctnwkueEzOzH25c4jl1IfWG0DsgB-CdYdbX2PUc70'
+    },
+
+];
+
